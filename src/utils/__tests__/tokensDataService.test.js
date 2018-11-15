a784aa9c2417fa312fd637ec669949b4a0b8fc56
Konstantin Gerasimov <konstantin@pillarproject.io>
2018-11-15 12:59:20 +0000

    Added algorithm to create asset.json.

--- src/utils/__tests__/tokensDataService.test.js
+++ src/utils/__tests__/tokensDataService.test.js
@@ -5,6 +5,10 @@ import {
 } from 'utils/tokensDataService';
 import coinMarketCapEthTokens from 'utils/__tests__/coinMarketCapEthTokens';
 import coinMarketCapEthTokensWithLinks from 'utils/__tests__/coinMarketCapEthTokensWithLinks';
+import coinMarkerCapTokensInfoMapBySymbol from 'utils/__tests__/coinMarkerCapTokensInfoMapBySymbol';
+import dataFromEtherscan from 'utils/__tests__/dataFromEtherscan';
+
+const fs = require('fs');
 
 it.skip('should getLinkToEtherscan() and write to file', async () => {
   jest.setTimeout(3000000);
@@ -18,4 +22,30 @@ it.skip('should getDataFromEtherscan() and write to file', async () => {
 
   const tokens = coinMarketCapEthTokensWithLinks.slice(0, 10);
   await downloadDataFromEtherscan(tokens);
+});
+
+it.skip('should write assets.json', () => {
+  const tokensMetadata = coinMarkerCapTokensInfoMapBySymbol;
+  const tokens = dataFromEtherscan.map((token) => {
+    const metadata = tokensMetadata[token.symbol];
+    return {
+      address: token.address,
+      decimals: parseInt(token.decimal, 10),
+      description: null,
+      email: null,
+      iconMonoUrl: null,
+      iconUrl: null,
+      isDefault: false,
+      isDefaultToken: false,
+      name: token.name,
+      symbol: token.symbol,
+      telegram: null,
+      twitter: metadata.urls.twitter[0] || null,
+      wallpaperUrl: null,
+      website: metadata.urls.website[0] || null,
+      whitepaper: null,
+    };
+  });
+
+  fs.writeFile('assets.json', JSON.stringify(tokens));
 });
