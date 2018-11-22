779d9c31bb0e2270ecce657629656f50e133b814
Konstantin Gerasimov <konstantin@pillarproject.io>
2018-11-22 15:24:48 +0000

    Final Tokens List

--- src/utils/__tests__/tokensDataService.test.js
+++ src/utils/__tests__/tokensDataService.test.js
@@ -7,10 +7,16 @@ import coinMarketCapEthTokens from 'utils/__tests__/coinMarketCapEthTokens';
 import coinMarketCapEthTokensWithLinks from 'utils/__tests__/coinMarketCapEthTokensWithLinks';
 import coinMarkerCapTokensInfoMapBySymbol from 'utils/__tests__/coinMarkerCapTokensInfoMapBySymbol';
 import dataFromEtherscan from 'utils/__tests__/dataFromEtherscan';
+
 import defaultAssets from 'utils/__tests__/defaultAssets';
 import iconsList from 'utils/__tests__/iconsList';
 import tokensIcons from 'utils/__tests__/tokensIcons';
 import erc20Icons from 'utils/__tests__/erc20Icons';
+
+import deltaTokens from 'utils/__tests__/delta';
+import unnecessaryTokens from 'utils/__tests__/removeThisTokens';
+
+import _finalTokensList from 'utils/__tests__/finalTokensList';
 
 const fs = require('fs');
 const sharp = require('sharp');
@@ -54,23 +60,6 @@ const toFinalToken = (token) => {
   };
 };
 
-const findDefaultSymbols = () => {
-  return defaultAssets.map(item => item.symbol.toLocaleLowerCase());
-};
-
-const getDataFromEtherscanMap = () => {
-  const dataFromEtherscanMap = {};
-  dataFromEtherscan.forEach(token => {
-    dataFromEtherscanMap[token.symbol.toLowerCase()] = token;
-  });
-  return dataFromEtherscanMap;
-};
-
-const findDeltaSymbols = () => {
-  const defaultSymbols = findDefaultSymbols();
-  return iconsList.filter(item => !defaultSymbols.includes(item));
-};
-
 it.skip('should downloadLinksFromCoinmarketcap() and write to file', async () => {
   jest.setTimeout(3000000);
 
@@ -90,48 +79,7 @@ it.skip('should write assets.json', () => {
   fs.writeFile('assets.json', JSON.stringify(tokens));
 });
 
-it.skip('should get delta icons', () => {
-  const dataFromEtherscanMap = {};
-  dataFromEtherscan.forEach(token => {
-    dataFromEtherscanMap[token.symbol.toLowerCase()] = token;
-  });
-
-  const tokens = findDeltaSymbols().map(symbol => {
-    return toFinalToken(dataFromEtherscanMap[symbol]);
-  });
-  console.log('delta icons:', tokens.length); // eslint-disable-line
-  fs.writeFile('delta.json', JSON.stringify(tokens));
-});
-
-// DELTA
-it.skip('should write just new tokens to delta.json', () => {
-  const defaultSymbols = findDefaultSymbols();
-  const filteredDataFromEtherscan = dataFromEtherscan
-    .filter(asset => !defaultSymbols.includes(asset.symbol.toLowerCase()));
-
-  const tokens = filteredDataFromEtherscan.map(token => {
-    return toFinalToken(token);
-  });
-
-  console.log('new tokens:', tokens.length);
-
-  fs.writeFile('delta.json', JSON.stringify(tokens));
-});
-
-it.skip('should ...', () => {
-  const dataFromEtherscanMap = getDataFromEtherscanMap();
-
-  const tokens = defaultAssets.map(asset => {
-    const data = dataFromEtherscanMap[asset.symbol.toLowerCase()];
-    const totalSupply = data ? data.totalSupply : null;
-    return {
-      ...asset,
-      totalSupply,
-    };
-  });
-
-  fs.writeFile('_tokens.json', JSON.stringify(tokens));
-});
+//
 
 it.skip('should return ERC20 icons', () => {
   const marketEthTokens = coinMarketCapEthTokens.map(token => token.symbol.toLocaleLowerCase());
@@ -176,18 +124,53 @@ it.skip('should save 64x64 resized icons x2', async () => {
 it.skip('should save 32x32 icons', async () => {
   jest.setTimeout(3000000);
 
-  const tokens = findDeltaSymbols();
+  const tokens = erc20Icons;
   tokens.forEach(async (token) => {
     await fs
       .createReadStream(`token_logo_black_128/${token}.png`)
       .pipe(sharp().resize(32, 32))
-      .pipe(fs.createWriteStream(`newTokensResized/${token}.png`));
+      .pipe(fs.createWriteStream(`newTokens/${token}.png`));
   });
 
   tokens.forEach(async (token) => {
     await fs
       .createReadStream(`token_logo_color_128/${token}.png`)
       .pipe(sharp().resize(32, 32))
-      .pipe(fs.createWriteStream(`newTokensResized/${token}Color.png`));
+      .pipe(fs.createWriteStream(`newTokens/${token}Color.png`));
   });
+});
+
+it.skip('should remove unnecessary tokens', () => {
+  console.log('tokens', deltaTokens.length);
+
+  const finalTokensList = deltaTokens.filter(token => !unnecessaryTokens.includes(token.symbol));
+
+  console.log('finalTokensList', finalTokensList.length);
+
+  fs.writeFile('finalTokensList.json', JSON.stringify(finalTokensList));
+});
+
+it.skip('should prepare data for BE', () => {
+  const finalTokensList = _finalTokensList.map(item => {
+    return {
+      address: item.address,
+      decimals: item.decimals,
+      description: item.description || null,
+      name: item.name,
+      symbol: item.symbol,
+      wallpaperUrl: item.wallpaperUrl || null,
+      iconUrl: item.iconUrl || null,
+      iconMonoUrl: item.iconMonoUrl || null,
+      email: item.email || null,
+      telegram: item.telegram || null,
+      twitter: item.twitter || null,
+      website: item.website || null,
+      whitepaper: item.whitepaper || null,
+      isDefault: item.isDefault || false,
+      isPreferred: item.isPreferred || false,
+    };
+  });
+
+  console.log('finalTokensList', finalTokensList.length);
+  fs.writeFile('_finalTokensList.json', JSON.stringify(finalTokensList));
 });
