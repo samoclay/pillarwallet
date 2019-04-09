342d1104cf34431a42f7d6c224a5e69e35073890
Antonio Nedanoski <antonio@tarmac.io>
2019-04-09 12:25:42 +0200

    Clean branch from master for Travis CI prod build

---
+++ build-and-upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $TRAVIS_BUILD_DIR
+brew install yarn
+yarn install
+yes | gem uninstall cocoapods
+gem install cocoapods -v 1.5.3
+cd $TRAVIS_BUILD_DIR/ios && pod install --verbose
+gem install bundler
+cd $TRAVIS_BUILD_DIR/ios && bundle check || bundle install --path vendor/bundle
+cd $TRAVIS_BUILD_DIR/ios && bundle update
+export buildNumber=$(cat $TRAVIS_BUILD_DIR/buildNumber.txt)
+export APP_BUILD_NUMBER=7785
+echo $FASTLANE_PASSWORD
+echo $OAUTH_TOKEN
+cd $TRAVIS_BUILD_DIR/ios && bundle exec fastlane deploy_staging APP_BUILD_NUMBER:$APP_BUILD_NUMBER build_number:$buildNumber APP_NAME:"Pillar Staging"
